209c184bf2f647b957be7129534b5d4ae2883c74
Wojciech Kaczmarek <frk@kofeina.net>
2013-07-08 16:21:45 +0200

    1st luna stuff

--- src/index.erl
+++ src/index.erl
@@ -2,68 +2,36 @@
 -module (index).
 -compile(export_all).
 -include_lib("nitrogen_core/include/wf.hrl").
--define(checkmark, <<16#2713/utf8>>).
-
-starts_w_foo(_Tag, _V=[$f,$o,$o|_]) -> true;
-starts_w_foo(_Tag, _V) -> false.
 
 main() -> #template { file="./site/templates/bare.html" }.
+debug_css() -> [].
+title() -> "luna.inthephase".
+desc() -> "All things lunar.".
+keywords() -> "moon, moon phases, luna, lunar".
 
-debug_css() ->
-    %% todo: turn on conditionally
-    "<link rel='stylesheet' href='/css/debug.css' />".
-
-title() -> "Welcome to Nitrogen".
-
-wire() ->
-    wf:session(test, v),
-    wf:wire(t1, #validate{
-        on=blur,
-        success_text=?checkmark,
-        validators= [
-            #is_required{text="<- required"},
-            #custom{text="must start with 'foo'", function=fun starts_w_foo/2},
-            #min_length{text="min 4 chars!!", length=4}
-        ]
-    }).
-
-body() ->
-    wire(),
+body() -> [
+    #br{},
     #container_16 { body=[
-        #grid_12 { prefix=2, suffix=2, alpha=true, omega=true,
-            body=inner_body()
-        }
-    ]}.
+        #grid_3 { prefix=2, alpha=true, body=logopix() },
+        #grid_7 { suffix=4, omega=true, style="outline:1px dotted",
+            body=logo()
+        },
+        #grid_clear{},
+        #grid_16{}
+    ]}
+].
 
-inner_body() -> 
-    [
-        #h1 { text="Welcome to Nitrogen" },
-        #p{},
-        "
-        If you can see this page, then your Nitrogen server is up and
-        running. Click the button below to test postbacks.
-        ",
-        #p{},
-        #grid_clear{},
-        #grid_2 { alpha=true, body=[
-            #button{ id=button, text="Click me!", 
-                postback=click }
-        ]},
-        #grid_4 { id=button_comment_box, omega=true, body=[]},
-        #grid_clear{},
-		#p{},
-        #textbox {id=t1, next=t2, placeholder="Foo", postback=t1pb},
-        #br{}, #textbox {id=t2, text="Bar"},
-        #p{},
-        "
-        Run <b>./bin/dev help</b> to see some useful developer commands.
-        ",
-		#p{},
-		"
-		<b>Want to see the ",#link{text="Sample Nitrogen jQuery Mobile Page",url="/mobile"},"?</b>
-		"
-    ].
-	
+logopix() ->
+    #image{ image="/images/Golden_Moon_small.jpg", style="width:120px" }.
+
+logo() -> [
+    #p{},
+    #span{ text="luna.inthephase",
+        style="font-size:56px; font-family:Tahoma,Arial,sans-serif" },
+    #p{}
+].
+
+
 event(t1pb) ->
     ?PRINT(t1pb);
 
