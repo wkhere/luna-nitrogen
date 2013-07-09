699ff48e474a44ffaea4821c6883fba14d80625a
Wojciech Kaczmarek <frk@kofeina.net>
2013-07-09 16:42:10 +0200

    dynamically modify background for specific logo pix; process dict version

--- src/index.erl
+++ src/index.erl
@@ -4,10 +4,33 @@
 -include_lib("nitrogen_core/include/wf.hrl").
 
 main() -> #template { file="./site/templates/bare.html" }.
-debug_css() -> [].
 title() -> "luna.inthephase".
 desc() -> "All things lunar.".
 keywords() -> "moon, moon phases, luna, lunar".
+
+
+setup_state() ->
+    Pix = rand_from_pixtab(),
+    put(chosen_pix, Pix),
+    [{css_mod, case Pix of 
+        "Solar_eclipse_May_2013" -> "body {background:#130101}";
+        _ -> []
+            end}].
+%% alt. version using ajax - process dict is not needed then:
+    %% case rand_from_pixtab() of 
+    %%     "Solar_eclipse_May_2013" ->
+    %%         wf:wire(#event{ type=timer, delay=0,
+    %%             actions= #script{ 
+    %%                 script="document.body.style.background=\"#130101\""
+    %%         }});
+    %%     _ -> nop
+    %% end
+
+
+modify_css() ->
+    St = setup_state(),
+    maybe(proplists:get_value(css_mod, St), "").
+    
 
 body() -> [
     #br{},
@@ -21,13 +44,20 @@ body() -> [
     ]}
 ].
 
-logopix() ->
+
+rand_from_pixtab() ->
     Pixtab = [
         "Golden_Moon", "Moonburn",
-        "Lunar_eclipse_June_2011", "Solar_eclipse_1999"
+        "Lunar_eclipse_June_2011", "Solar_eclipse_1999",
+        "Solar_eclipse_May_2013"
     ],
+    rand_element(Pixtab).
+
+
+logopix() ->
+    Pix = maybe(get(chosen_pix), "Moonburn"),
     #image{
-        image="/images/" ++ rand_element(Pixtab) ++ "_small.jpg",
+        image="/images/" ++ Pix ++ "_small.jpg",
         style="height:100px" }.
 
 logo() -> [
@@ -42,3 +72,6 @@ logo() -> [
 
 rand_element(Xs) ->
     lists:nth( random:uniform(length(Xs)), Xs ).
+
+maybe(undefined, Default) -> Default;
+maybe(X, _) -> X.
